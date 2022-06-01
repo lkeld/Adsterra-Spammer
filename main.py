7403533d4be0548cc70bc24b903b11f8335de063
Luke Eldridge <48638597+lkeld@users.noreply.github.com>
2022-06-02 09:51:22 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from selenium import webdriver
 import chromedriver_autoinstaller
 from selenium.webdriver.chrome.options import Options
 
-#Sets Chromedriver as headless
+#Sets Chromedriver Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument(f'--proxy-server={proxy})
 
 # Loads Website
 driver = webdriver.Chrome()
@@ -13,6 +14,18 @@ driver.get("https://lukedev.me/hosting/spamthis.html")
 
 # Load Proxies
 f = open("proxies.txt", "r")
+list_of_lines = f.readlines()
+if not any("x " in s for s in list_of_lines): # add locator to first item in file when running for first the time
+    list_of_lines[0] = "x " + list_of_lines[0]
+for index, line in enumerate(list_of_lines):
+    if "x " in line:
+        next_index = index + 1
+        if index == len(list_of_lines) -1:
+            next_index = 0
 
+        list_of_lines[index] = list_of_lines[index].split("x ").pop() # update current line
+        proxy = list_of_lines[index]
+        list_of_lines[next_index] = "x " + list_of_lines[next_index] # update next line
+        break
 
 
