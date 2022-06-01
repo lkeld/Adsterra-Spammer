835b107a36ae76570df8a89411634fa395accc5a
Luke Eldridge <48638597+lkeld@users.noreply.github.com>
2022-06-02 09:49:17 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import selenium
+from selenium import webdriver
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
+
+#Sets Chromedriver as headless
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+# Loads Website
+driver = webdriver.Chrome()
+driver.get("https://lukedev.me/hosting/spamthis.html")
+
+# Load Proxies
+f = open("proxies.txt", "r")
+
 
 
